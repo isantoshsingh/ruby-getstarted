2446545b9a5001ae24a54e80645ec9c27e35ab8d
Santosh Singh <santosh@antstack.com>
2016-01-26 22:58:59 +0530

    new commits

--- Greeter.rb
+++ Greeter.rb
@@ -1,4 +1,5 @@
 class Greeter
+  attr_accessor :name
   def initialize(name = "Santosh")
     @name = name
   end
@@ -11,5 +12,22 @@ class Greeter
 end
 
 greeter = Greeter.new("India")
+#greeter = Greeter.new
 greeter.say_hi
 greeter.say_bye
+
+#Greeter.say_hi
+puts greeter.__id__
+
+puts greeter.instance_of?Greeter
+
+puts greeter.nil?
+
+#get methods of class excluding ancestors
+puts Greeter.instance_methods(false)
+
+
+puts greeter.is_a?Greeter
+
+puts greeter.respond_to?('say_hi')
+puts greeter.respond_to?('to_s')
---
+++ one_liner.rb
@@ -0,0 +1,4 @@
+times_2 = 2
+times_2 *=2 while times_2 < 100
+
+puts times_2
